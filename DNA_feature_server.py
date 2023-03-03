56bc0183e18db11d94ea9c9d1afb50dc39a6db2a
Aleksandr Ivanov <alekzivanov@gmail.com>
2023-03-03 19:31:09 +0300

    Подправлена форма загрузки ДНК

--- DNA_feature_server.py
+++ DNA_feature_server.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index(): #обрабатываем главную страницу
     title = "DNA feature finder"
-    
+     
     return render_template('index.html', page_title=title, querry_length = 777, remove_count=0)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
