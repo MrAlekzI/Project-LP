8fc368c1420d0edc1974da785978c62c4849399a
Aleksandr Ivanov <alekzivanov@gmail.com>
2023-03-04 15:15:59 +0300

    Добавлена загрузки последовательности в переменную и обработка ее длины

--- DNA_feature_server.py
+++ DNA_feature_server.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET', 'POST'])
 def index(): #обрабатываем главную страницу
-    title = "DNA feature finder"
-     
-    return render_template('index.html', page_title=title, querry_length = 777, remove_count=0)
-
+    try:
+        dna_querry = '' #чтобы страница не выдвала ошибку, почему то не видит если вынести в глобальные
+        title = "DNA feature finder"
+        if request.method == 'POST':   
+            dna_querry = request.form.get('dna_querry') 
+            dna_querry_lengh=len(dna_querry) 
+            return render_template('index.html', page_title=title, querry_length = dna_querry_lengh, remove_count=0,)
+    except TypeError: #пока последовательность не введена почему то dna_qurry ==None
+        return render_template('index.html', page_title=title, querry_length = '', remove_count=0,)
+         
 
 if __name__ == '__main__':
     app.run(debug=True)
