4ebecc5fd1178316fb744f3883a0e584ad2e888c
Aleksandr Ivanov <alekzivanov@gmail.com>
2023-03-16 21:09:26 +0300

    Verification of query lengh from DB

--- genebank.py
+++ genebank.py
@@ -7,14 +7,37 @@
 
 from Bio import Entrez, SeqIO
 
+class DNALengthError(ValueError): #сделал ошибки чтобы при их возникновении веб быстрее их обрабатывал
+    pass
+class NonDNAError(TypeError):
+    pass
+
+
 Entrez.email = 'somemailsomemail@gmail.cpm' #необходим любой email чтобы больше запросов длеать
 
-def genebank_querry(ID:int) -> str:
+def validation(ID:str) -> dict:
+    
+    handle = Entrez.efetch(db="nucleotide", id=f'{ID}', rettype="gb", retmode="text") #хэндрел из другого типа описания, где содержится длин
+    dna_decription = handle.readline().split()
+    length= int(dna_decription[2])
+    nucleotide_type = dna_decription[4]
+    query = {'ID':ID, 'length':length, 'nucl_type':nucleotide_type}
+    return query
+
+def genebank_querry(ID:str) -> str:
     handle = Entrez.efetch(db="nucleotide", id=f"{ID}", rettype="fasta", retmode='text') #создается хэндлей и с нип проводятся дальнейшие манипуляции
-    record = SeqIO.read(handle, 'fasta')
-    sequence = str(record.seq)
-    return sequence
+    if validation(ID)['length'] <= 100000:
+        record = SeqIO.read(handle, 'fasta')
+        sequence = str(record.seq)
+        return sequence
+    else:
+        raise DNALengthError (f'The lengh of the query:{ID} is longer than 100 kb!')
+    
+def isRNA(ID): #чтобы сообщить что введена последовательноть РНК, это не критично но иногда следует знать это
+    if validation(ID)['nucl_type'].find('RNA') != -1: #могут быть записаны RNA, mRNA, rRNA, tRNA поэтому ищем общее
+        raise NonDNAError (f'AttentionThe query: {ID} contains RNA sequence')
 
 if __name__ == '__main__':
     ID = input()
     print(genebank_querry(ID))
+    print(isRNA(ID))
