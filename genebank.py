1ac7df3bcc3ec465c7699718223a8a5ea388d78d
Aleksandr Ivanov <alekzivanov@gmail.com>
2023-03-28 11:20:58 +0300

    changes after review 2

--- genebank.py
+++ genebank.py
@@ -2,7 +2,7 @@
 для работы необходим пакет biopython
 при подключении по ID последовательности выдается объетк Bio.bio.seq из которго можно получить строку
 
-примеры  ID :NM_018094, M_001374734.1, NC_052532.1 (1 млн), CM021573.2 (170 млн - может долго грузить)
+примеры  ID :NM_018094, NM_001374734.1, NC_052532.1 (1 млн), CM021573.2 (170 млн - может долго грузить)
 '''
 
 from Bio import Entrez, SeqIO        
@@ -13,11 +13,15 @@ class NonDNAError(TypeError):
     pass
 
 class Nucleotide:
-    def __init__(self, id_seq, length, nuc_type, sequence):
+    def __init__(self,id_seq):
         self.id_seq = id_seq
-        self.length = length
-        self.nuc_type = nuc_type
-        self.sequence = sequence
+        self.dna_description = id_parsing(self.id_seq) 
+        self.length = int(self.dna_description[2])
+        self.nuc_type = self.dna_description[4]
+        if self.length <= 100000:
+            self.sequence = genebank_querry(self.id_seq)
+        else:
+            raise DNALengthError (f'The lengh of the query:{id_seq} is longer than 100 kb!')
 
     def __repr__(self):
         return f'The query contains {self.nuc_type} with length {self.length}'
@@ -33,28 +37,23 @@ class Nucleotide:
 
 Entrez.email = 'somemailsomemail@gmail.cpm' #необходим любой email чтобы больше запросов длеать
 
-def id_parsing(id_seq:str) -> object:
-    
+def id_parsing(id_seq:str) -> str:
     handle = Entrez.efetch(db="nucleotide", id=id_seq, rettype="gb", retmode="text") #хэндрел из другого типа описания, где содержится длин
-    dna_decription = handle.readline().split() #первая строка опсиания гена из базы данных Gene bank где через знаки табуляции тип гена, id, длина, тип полимера, топология, дата обновления 
-    length= int(dna_decription[2])
-    nucleotide_type = dna_decription[4]
-    nucl_query = Nucleotide(id_seq, length, nucleotide_type, None)
-    return nucl_query
+    dna_description = handle.readline().split() #первая строка опсиания гена из базы данных Gene bank где через знаки табуляции тип гена, id, длина, тип полимера, топология, дата обновления
+    return dna_description
 
 def genebank_querry(id_seq:str) -> str:
-    dna_obj = id_parsing(id_seq) #gпарсим и создаем объект
-    if dna_obj.validation():
-        handle = Entrez.efetch(db="nucleotide", id=id_seq, rettype="fasta", retmode='text') #создается хэндлей и с нип проводятся дальнейшие манипуляции
-        record = SeqIO.read(handle, 'fasta')
-        sequence = str(record.seq)
-        dna_obj.sequence = sequence #дописываем в объект атрибут
-        return dna_obj 
-    else:
-        raise DNALengthError (f'The lengh of the query:{dna_obj.id_seqid_seq} is longer than 100 kb!')
+    #gb_handle = id_parsing(id_seq) #парсим и cсоздаем первый хэндл с описанием гена
+    #dna_obj = Nucleotide(id_seq, None)
+    #if dna_obj.validation():
+    handle = Entrez.efetch(db="nucleotide", id=id_seq, rettype="fasta", retmode='text') #создается хэндлей который уже содержит информацию о последвоательности и с нип проводятся дальнейшие манипуляции
+    record = SeqIO.read(handle, 'fasta')
+    return str(record.seq)
+        
     
 
 if __name__ == '__main__':
     id = input()
-    print(genebank_querry(id).sequence)
-    print(genebank_querry(id).type_check())
+    dna_query = Nucleotide(id)
+    print(dna_query.sequence)
+    print(dna_query.type_check())
